efd7106d07db487ef891069751bfbd9c8be029ed
PythonTrainingSP <pythontraning737@gmail.com>
2022-05-11 22:27:26 +0530

    added new file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent} from './app.component';
+import { ProductComponent } from './product/product.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ProductComponent]
 })
-export class AppModule { }
+export class AppModule { } 
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+    name = 'IPhone ME';
+    constructor() { }
+    ngOnInit(): void {
+    }
+}
